1fd380528f2716bee1d780e4a0474168c6f21768
Oleg Voitko <pel6men6.oleg@gmail.com>
2023-02-01 16:08:55 +0300

    feat: add poker table & buttons

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Table_poker from './components/Poker-table/Poker_table';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path='' element={<Table_poker />}></Route>
+      </Routes>
+    </>
   );
 }
 
