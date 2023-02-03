28198eaaaa932faa10ff6ea7ad2c1ef91ff87e86
Maksim1509 <maaa23@mail.ru>
2023-02-03 15:09:58 +0400

    feat: add chat to table room

--- frontend/src/pages/TableRoom.tsx
+++ frontend/src/pages/TableRoom.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import Chat from '../components/Chat/Chat';
+import Table_poker from '../components/Poker-table/Poker_table';
+import './TableRoom.scss';
 
 const TableRoom = (): JSX.Element => {
   return (
     <>
       <h1>TableRoom</h1>
+      <section className='tableroom__table'>
+        <Table_poker />
+      </section>
+      <section className='tableroom__chat'>
+        <Chat />
+      </section>
     </>
   );
 };
