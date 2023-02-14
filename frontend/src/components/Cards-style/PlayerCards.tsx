5dc382c63f89cda814bbdd4a3211ba5de10a60b6
Oleg Voitko <pel6men6.oleg@gmail.com>
2023-02-14 14:37:59 +0300

    feat: add avatars png

--- frontend/src/components/Cards-style/PlayerCards.tsx
+++ frontend/src/components/Cards-style/PlayerCards.tsx
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 import * as React from 'react';
 import { IUser } from 'types/interfaces';
 import { Suit } from './Suit';
-
+import { useAppSelector } from '../../hooks/hook';
 const CardWrapper = styled.div`
   background-color: white;
   border-radius: 8px;
@@ -65,44 +65,66 @@ interface PlayersProps {
   users: IUser[];
 }
 
-export const RenderPlayer: React.FC<PlayersProps> = ({ users }) => (
-  <>
-    {users.map((u, i) => (
-      <div className={`${'player'} ${'p' + [i]}`} key={i}>
-        <div className='player-two-cards'>
-          <div className='playing-card1'>
-            <CardWrapper data-suit={u.gameState.hand[0].suit}>
-              <span className='cardInfo top'>
-                <div>{u.gameState.hand[0].cardFace}</div>
-                <Suit suit={u.gameState.hand[0].suit} />
-              </span>
-              <div className='cardSuit'>
-                <Suit suit={u.gameState.hand[0].suit} />
-              </div>
-            </CardWrapper>
+const imageAvatarUsers: string[] = [
+  'bug.png',
+  'congrats.png',
+  'deadline.png',
+  'expert.png',
+  'finished.png',
+  'mentor.png',
+  'without_mentor.png',
+  'works.png',
+  'writing.png',
+  'wtf.png',
+];
+
+// const img = (array: string | any[]) => {
+//   const randomNum = Math.floor(Math.random() * array.length);
+//   return array[randomNum];
+// };
+
+export const RenderPlayer: React.FC<PlayersProps> = ({ users }) => {
+  const user = useAppSelector((state) => state.user.user) as IUser;
+  return (
+    <>
+      {users.map((u, i) => (
+        <div className={`${'player'} ${'p' + [i]}`} key={i}>
+          <div className='player-two-cards'>
+            <div className='playing-card1'>
+              <CardWrapper data-suit={u.gameState.hand[0].suit}>
+                <span className='cardInfo top'>
+                  <div>{u.gameState.hand[0].cardFace}</div>
+                  <Suit suit={u.gameState.hand[0].suit} />
+                </span>
+                <div className='cardSuit'>
+                  <Suit suit={u.gameState.hand[0].suit} />
+                </div>
+              </CardWrapper>
+            </div>
+            <div className='playing-card2'>
+              <CardWrapper data-suit={u.gameState.hand[1].suit}>
+                <span className='cardInfo top'>
+                  <div>{u.gameState.hand[1].cardFace}</div>
+                  {/* {user._id === u._id && (<Suit suit={u.gameState.hand[1].suit} />)} */}
+                  <Suit suit={u.gameState.hand[1].suit} />
+                </span>
+                <div className='cardSuit'>
+                  <Suit suit={u.gameState.hand[1].suit} />
+                </div>
+              </CardWrapper>
+            </div>
           </div>
-          <div className='playing-card2'>
-            <CardWrapper data-suit={u.gameState.hand[1].suit}>
-              <span className='cardInfo top'>
-                <div>{u.gameState.hand[1].cardFace}</div>
-                <Suit suit={u.gameState.hand[1].suit} />
-              </span>
-              <div className='cardSuit'>
-                <Suit suit={u.gameState.hand[1].suit} />
-              </div>
-            </CardWrapper>
+          <div className='player-avatar-container'>
+            <img
+              className='player-avatar_img'
+              src={`${require('../../assets/avatar/mentor.png')}`}
+              alt='Avatar'
+            />
           </div>
+          <h4 className='player-name'>{user.nickname}</h4>
+          <h4 className='player-stack'>$ {u.gameState.stack}</h4>
         </div>
-        <div className='player-avatar-container'>
-          <img
-            className='player-avatar_img'
-            src={require('../../assets/deadline.png')}
-            alt='Avatar'
-          />
-        </div>
-        <h4 className='player-name'>Oleg</h4>
-        <h4 className='player-stack'>$ {u.gameState.stack}</h4>
-      </div>
-    ))}
-  </>
-);
+      ))}
+    </>
+  );
+};
